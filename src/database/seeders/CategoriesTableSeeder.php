a286613ab528dc3f29f4c0ad9634cdab6cca0735
hisashi mamada <rm8422gitabero@gmail.com>
2025-03-25 18:54:58 +0900

    修正点を反映（再提出）

--- src/database/seeders/CategoriesTableSeeder.php
+++ src/database/seeders/CategoriesTableSeeder.php
@@ -16,11 +16,9 @@ class CategoriesTableSeeder extends Seeder
     public function run(): void
     {
         DB::table('categories')->insert([
-            ['content' => '商品のお届けについて', 'created_at' => now(), 'updated_at' => now()],
-            ['content' => '商品の交換について', 'created_at' => now(), 'updated_at' => now()],
-            ['content' => '商品トラブル', 'created_at' => now(), 'updated_at' => now()],
-            ['content' => 'ショップへのお問い合わせ', 'created_at' => now(), 'updated_at' => now()],
-            ['content' => 'その他', 'created_at' => now(), 'updated_at' => now()],
+            ['content' => '商品の交換について', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['content' => 'サービスの不具合', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['content' => 'その他', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
         ]);
     }
 }
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class CategoriesTableSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
